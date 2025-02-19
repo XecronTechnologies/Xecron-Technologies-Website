a3311c13bb8826df37a854a67aef543545587dd2
XecronTechnologies <xecrontech@gmail.com>
2025-02-19 18:34:20 +0000

    Modified Maintanence Outlook

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home'
 
+const maintanence = '/Maintanence.gif'
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
-      <p>Site is Under Maintanence</p>
+      <img src={maintanence} alt="maintanence" />
+      <h1>Site is Under Maintanence</h1>
+      {/* <Home /> */}
     </>
   )
 }
