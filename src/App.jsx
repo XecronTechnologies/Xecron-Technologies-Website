c9b1dfd0a2702ad436b6364885e9db427e584ad6
XecronTechnologies <xecrontech@gmail.com>
2025-02-20 17:23:09 +0000

    Tried SEO with robots.txt,sitemap.xml and changes in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ import NavigationBar from './components/NavigationBar'
 
 const maintanence = '/Maintanence.gif'
 function App() {
-  const [count, setCount] = useState(0)
-
-
   return (
     <>
-    <NavigationBar />
+    {/* <NavigationBar /> */}
 
-      <img src={maintanence} alt="maintanence" />
-      <h1>Site is Under Maintanence</h1>
+      <center><img src={maintanence} alt="maintanence" /></center>
+      <h1>This Site is Under Maintenance</h1>
       {/* <Home /> */}
     </>
   )
