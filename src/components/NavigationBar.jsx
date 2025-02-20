f56fd206d15b26a0f61e28cf921d2dd69f810688
XecronTechnologies <xecrontech@gmail.com>
2025-02-20 16:21:19 +0000

    Created Nav bar for Both View

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,8 +4,8 @@ import logo from '/xecron-logo.png';
 import { navItems } from '../constants';
 
 const NavigationBar = () => {
-    const [mobileDrawerOpen,setMobileDrawerOpen] = useState(false);
-    const toggleNavBar = () =>{
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+    const toggleNavBar = () => {
         setMobileDrawerOpen(!mobileDrawerOpen);
     }
 
@@ -37,25 +37,31 @@ const NavigationBar = () => {
                         </a>
                     </div>
 
-                        {/* Mobile Drawer */}
-                        <div className="lg:hidden md:flex flex-col justify-end">
-                            <button onClick={toggleNavBar}>
-                                {mobileDrawerOpen ? <X /> : <Menu />}
-                            </button>
-                        </div>
+                    {/* Mobile Drawer */}
+                    <div className="lg:hidden md:flex flex-col justify-end">
+                        <button onClick={toggleNavBar}>
+                            {mobileDrawerOpen ? <X /> : <Menu />}
+                        </button>
+                    </div>
                 </div>
 
                 {mobileDrawerOpen && (
                     <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
                         <ul>
                             {navItems.map((item, index) => (
-                                <li key={index}>
+                                <li key={index} className="py-4">
                                     <a href={item.href}>{item.label}</a>
                                 </li>
                             ))}
                         </ul>
+                        <div className="flex space-x-6 py-5">
+                            <a href="#" className="py-2 px-3 border rounded-md">
+                                Sign In
+                            </a>
+                            <a href="#" className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800">Create an Account</a>
+                        </div>
                     </div>
-                    )}
+                )}
 
             </div>
         </div>
