0c8814f9d95ba6ac8559e97637f3a4b457d4987d
XecronTechnologies <xecrontech@gmail.com>
2025-02-23 09:33:55 +0000

    Created Workflow Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,15 @@ import {footerLinks} from '../constants/index'
 const Footer = () => {
   return (
     <>
-    <footer className="mt-20 border-t py-10 border-neutral-700">
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+    <footer className="mt-20 border-t m-40 py-10 border-neutral-700">
+      <div className="text-left grid grid-cols-2 lg:grid-cols-3 gap-4">
         {/* Resources */}
-          <div className=" h3 text-md mb-4">
+          <div className="h3 text-md mb-4">
             <h3 className="text-md py-5  font-semibold ">Resources</h3>
             <ul className="space-y-2 ">
               {footerLinks.map((link,index)=>(
-                <li key={index}>
-                  <a className="text-left text-neutral-300 hover-text-white" href={link.href}>{link.text}</a>
+                <li key={index} className="text-neutral-500 ">
+                  <a className="text-neutral-300 hover-text-white" href={link.href}>{link.text}</a>
                 </li>
               ))}
             </ul>
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,62 +1,87 @@
-import {BotMessageSquare} from "lucide-react";
-import {BatteryCharging} from "lucide-react"
-import {Fingerprint} from "lucide-react";
-import {ShieldHalf} from "lucide-react";
-import {PlugZap} from "lucide-react";
+import { BotMessageSquare } from "lucide-react";
+import { BatteryCharging } from "lucide-react"
+import { Fingerprint } from "lucide-react";
+import { ShieldHalf } from "lucide-react";
+import { PlugZap } from "lucide-react";
 // import {GLobeLock} from "lucide-react";
 
-   
+
 // Navigation bar Items
 export const navItems = [
-    {label: "Features", href:"#"},
-    {label: "Workflow", href:"#"},
-    {label: "Pricing", href:"#"},
-    {label: "Testimonials", href:"#"},
+    { label: "Features", href: "#" },
+    { label: "Workflow", href: "#" },
+    { label: "Pricing", href: "#" },
+    { label: "Testimonials", href: "#" },
 ];
+
 
 //Features Items
 export const features = [
     {
         icon: <BotMessageSquare />,
-        text:"Drag-and-Drop Interface",
-        description :"Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
+        text: "Drag-and-Drop Interface",
+        description: "Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
     },
     {
         icon: <BotMessageSquare />,
-        text:"Drag-and-Drop Interface",
-        description :"Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
+        text: "Drag-and-Drop Interface",
+        description: "Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
     },
     {
         icon: <BotMessageSquare />,
-        text:"Drag-and-Drop Interface",
-        description :"Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
+        text: "Drag-and-Drop Interface",
+        description: "Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
     },
     {
         icon: <BotMessageSquare />,
-        text:"Drag-and-Drop Interface",
-        description :"Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
+        text: "Drag-and-Drop Interface",
+        description: "Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
     },
     {
         icon: <BotMessageSquare />,
-        text:"Drag-and-Drop Interface",
-        description :"Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
+        text: "Drag-and-Drop Interface",
+        description: "Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
     },
     {
         icon: <BotMessageSquare />,
-        text:"Drag-and-Drop Interface",
-        description :"Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
+        text: "Drag-and-Drop Interface",
+        description: "Easl y esugn an arrange your VR environments Wit a user— nen dra —and-dro interface.",
     },
 ]
 
+//Check List Items
+export const checklistItems = [
+    {
+        title: "AI Assistance to reduce time",
+        description:
+            "Xecron Technologies excels in diverse tech solutions, crafting robust digital futures with innovation.",
+    },
+    {
+        title: "AI Assistance to reduce time",
+        description:
+            "Xecron Technologies excels in diverse tech solutions, crafting robust digital futures with innovation.",
+    },
+    {
+        title: "AI Assistance to reduce time",
+        description:
+            "Xecron Technologies excels in diverse tech solutions, crafting robust digital futures with innovation.",
+    },
+    {
+        title: "AI Assistance to reduce time",
+        description:
+            "Xecron Technologies excels in diverse tech solutions, crafting robust digital futures with innovation.",
+    },
 
+
+]
 
 // Footer Resources Link Items
 export const footerLinks = [
-    {href:"#",text:"Home"},
-    {href:"#",text:"About Us"},
-    {href:"#",text:"Services"},
-    {href:"#",text:"Community"},
-    {href:"#",text:"Support"},
+    { href: "#", text: "Home" },
+    { href: "#", text: "About Us" },
+    { href: "#", text: "Services" },
+    { href: "#", text: "Community" },
+    { href: "#", text: "Support" },
 ]
 
 
